a7f2d9516e45db18bc6df4e8aa4af241965347a2
Tejasvi <tejasvi@tejasvis-macbook-air.local>
2018-03-24 15:01:06 +0530

    Initial Commit

---
+++ youtube emded/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  youtube emded
+//
+//  Created by Tejasvi on 24/03/18.
+//  Copyright © 2018 yiit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
